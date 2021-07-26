bcce9726eb48a99c9b9c81b4e6530a9dfa4ba5be
castell439501 <sinan.olgun@icloud.com>
2021-07-26 15:41:15 +0200

    refactor: Print which month is selected

--- Bikeshare.py
+++ Bikeshare.py
@@ -40,6 +40,7 @@ def get_filters():
         month_input=input('Please choose a month between January and June: or type in all: \n')
         if month_input.lower() in MONTH_DATA:
             month=month_input.lower()
+            print("The month you have selected is:"+month)
         else:
             print("The month you have selected is not valid. Please try again \n")
     # get user input for day of week (all, monday, tuesday, ... sunday)
